bd91a31b4b00df2470d49e38a1281edc66501242
Samridhi384 <samridhi.a@crestinfosystems.com>
2024-05-29 13:56:52 +0530

    updation1

--- testes/helpers/page.js
+++ testes/helpers/page.js
@@ -4,7 +4,10 @@ const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
